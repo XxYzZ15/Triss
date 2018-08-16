781957e56488f55018ed8206b782200fb98c5a10
XxYzZ15 <trissmoretz@gmail.com>
2018-08-16 20:52:38 +0800

    Add files via upload

---
+++ admin/appointment.php
@@ -0,0 +1,20 @@
+<?php include "includes/admin_header.php"; ?>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <?php include"includes/admin_navigation.php"; ?>
+        
+        
+        
+        
+        
+
+        
+        
+        
+        
+        
+        <!-- /#page-wrapper -->
+
+   <?php include"includes/admin_footer.php"; ?>
---
+++ admin/includes/admin_header.php
@@ -0,0 +1,56 @@
+<?php include "../includes/db.php" ?>;
+<?php include "functions.php"; ?>
+<?php ob_start(); ?>
+<?php session_start(); ?>
+
+<?php
+    if(isset($_SESSION['user_role'])){
+        
+    if($_SESSION['user_role'] !== 'admin'){
+        
+        header("Location: ../index.php");
+        
+    }
+        
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Admin Form</title>
+
+   <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+     
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/sb-admin.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
